cc6ddde74acd353d7de7d06396f8bf32e9755dcb
hison731 <jinxing4@outlook.com>
2019-08-29 07:50:51 +0800

    update

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,8 +1,26 @@
 import React from "react"
 
 function MainContent() {
+    const firstName = "Bob"
+    const lastName = "Ziroll"
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+
+    if (hours < 12) {
+        timeOfDay = "morning"
+    } else if (hours >= 12 && hours < 17) {
+        timeOfDay = "afternoon"
+    } else {
+        timeOfDay = "night"
+    }
+
     return (
         <div>
+            <h1>It is currently about {date.getHours() % 12} o'clock!</h1>
+            <h1 style={{color: "#FF8C00", }}>Good {timeOfDay}</h1>
+            <h1>Hello {firstName + " " + lastName}!</h1>
+            <h1>Hello {`${firstName} ${lastName}`}!</h1>
             <input type="checkbox" />
             <p>Placeholder text here</p>
 
