821532df962bac8942df9bfae8ee47105f4ded5b
hison731 <jinxing4@outlook.com>
2019-08-29 02:36:12 +0800

    components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+
+import Header from "./components/Header"
+import MainContent from "./components/MainContent"
+import Footer from "./components/Footer"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <div id="myinfo"></div>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
 }
 
-export default App;
+export default App
