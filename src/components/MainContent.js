fdb8f9c3c8d8d77180ca62caf81af3e0c99ac268
hison731 <jinxing4@outlook.com>
2019-08-29 02:43:50 +0800

    styling

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,7 +2,19 @@ import React from "react"
 
 function MainContent() {
     return (
-        <main>This is the main section</main>
+        <div>
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
+
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
+
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
+
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
+        </div>
     )
 }
 
