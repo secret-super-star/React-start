27bf102f3a9031e33f495fa94114708b40fb65b2
hison731 <jinxing4@outlook.com>
2019-08-29 08:09:32 +0800

    update

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react"
 import Header from "./components/Header"
 import MainContent from "./components/MainContent"
 import Footer from "./components/Footer"
+import Card from "./components/card"
 
 function App() {
     return (
         <div>
             <Header />
             <MainContent />
+            <Card />
             <Footer />
         </div>
     )
